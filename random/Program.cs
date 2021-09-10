650de54ec0503f51151e3154f4fb40b5cebd55af
Adas <adam.misiag@live.com>
2021-09-10 13:15:29 +0200

    Początkowe zaewidencjonowanie modułu random

---
+++ random/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace random
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
